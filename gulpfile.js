c513f2c2a7c57a1aa888811045602fa78edcbfbe
h-n-y <hnyelek@gmail.com>
2016-11-06 12:05:54 -0700

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 gulp.task('default', function() {
   console.log('GULP GULP GULP');
@@ -51,4 +55,54 @@ gulp.task('imageResize', function() {
         }))
         .pipe(gulp.dest(destImgPath));
 
+});
+
+gulp.task('minify-css', function() {
+  gulp.src('src/css/*.css')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dist/css/'));
+  gulp.src('src/views/css/*.css')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dist/views/css/'))
+});
+
+gulp.task('minify-html', function() {
+  gulp.src('src/*.html')
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      }))
+      .pipe(gulp.dest('dist'));
+  gulp.src('src/views/*.html')
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      }))
+      .pipe(gulp.dest('dist/views/'));
+});
+
+gulp.task('minify-js', function(err) {
+  pump([gulp.src('src/js/*.js'),
+        uglify(),
+        gulp.dest('dist/js/')],
+      err);
+
+  pump([gulp.src('src/views/js/*.js'),
+        uglify(),
+        gulp.dest('dist/views/js/')],
+      err);
+});
+
+// Watchers
+var watchHTML = gulp.watch(['src/*.html', 'src/views/*.html'], ['minify-html']);
+var watchCSS = gulp.watch(['src/css/*.css', 'src/views/css/*.css'], ['minify-css']);
+var watchJS = gulp.watch(['src/js/*.js', 'src/views/js/*.js'], ['minify-js']);
+[watchHTML, watchCSS, watchJS].forEach(function(watcher) {
+  watcher.on('change', function(event) {
+    console.log('GULP WATCHER');
+    console.log('FILE: ' + event.path);
+    console.log('TYPE: ' + event.type);
+  });
 });
